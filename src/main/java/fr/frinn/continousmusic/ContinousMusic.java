9d4e6a70a96f6d69ba24895e74211f9556cd19ee
Frinn38 <32675710+Frinn38@users.noreply.github.com>
2020-04-22 14:59:40 +0200

    Fabric 1.15

--- src/main/java/fr/frinn/continousmusic/ContinousMusic.java
+++ src/main/java/fr/frinn/continousmusic/ContinousMusic.java
@@ -2,38 +2,33 @@ package fr.frinn.continousmusic;
 
 import java.lang.reflect.Field;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.crash.CrashReport;
-import net.minecraft.crash.ReportedException;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.crash.CrashException;
+import net.minecraft.util.crash.CrashReport;
 
-@Mod(ContinousMusic.MODID)
-public class ContinousMusic {
+public class ContinousMusic implements ClientModInitializer {
 	
 	public static final String MODID = "infinitemusic";
 	
-	public static Minecraft mc = Minecraft.getInstance();
+	public static MinecraftClient mc = MinecraftClient.getInstance();
 	
 	public static MusicTickHandler ticker = new MusicTickHandler(mc);
-	
-	public ContinousMusic() {
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
-	}
 
-    public void clientSetup(FMLClientSetupEvent event) {
-    	//Replace the vanilla music ticker with custom one
-    	try {
-    		for(Field f : mc.getClass().getDeclaredFields()) {
-    			if(f.getName().equals("mcMusicTicker") || f.getName().equals("field_147126_aw")) {
-    				f.setAccessible(true);
-    				f.set(mc, ticker);
-    			}
-    		}
-    	}
-    	catch(Exception e) {
-    		throw new ReportedException(new CrashReport("Music Choices couldn't load in it's music ticker! Things won't work.", e));
-    	}    
+	@Override
+	public void onInitializeClient() {
+		//Replace the vanilla music ticker with custom one
+		try {
+			for(Field f : mc.getClass().getDeclaredFields()) {
+				if(f.getName().equals("musicTracker") || f.getName().equals("field_1714")) {
+					f.setAccessible(true);
+					f.set(mc, ticker);
+					System.out.println("Infinite Music Ticker set !");
+				}
+			}
+		}
+		catch(Exception e) {
+			throw new CrashException(new CrashReport("Music Choices couldn't load in it's music ticker! Things won't work.", e));
+		}
 	}
 }
--- src/main/java/fr/frinn/continousmusic/MusicTickHandler.java
+++ src/main/java/fr/frinn/continousmusic/MusicTickHandler.java
@@ -2,35 +2,37 @@ package fr.frinn.continousmusic;
 
 import java.util.Random;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.audio.*;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.sound.MusicTracker;
+import net.minecraft.client.sound.PositionedSoundInstance;
+import net.minecraft.client.sound.SoundInstance;
 
-public class MusicTickHandler extends MusicTicker {
-	
-	private final Random rand = new Random();
-	private final Minecraft mc;
-    private ISound currentMusic;
+public class MusicTickHandler extends MusicTracker {
+
+	private final MinecraftClient mc;
+    private SoundInstance currentMusic;
     private int timeUntilNextMusic = 20;
 
-	public MusicTickHandler(Minecraft mc) {
+	public MusicTickHandler(MinecraftClient mc) {
 		super(mc);
 		this.mc = mc;
 	}
 	
 	@Override
 	public void tick() {
+	    System.out.println(timeUntilNextMusic);
 		
-		MusicTicker.MusicType musictype = this.mc.getAmbientMusicType();
+		MusicTracker.MusicType musictype = this.mc.getMusicType();
 
         if (this.currentMusic != null)
         {
-            if (!musictype.getSound().getName().equals(this.currentMusic.getSoundLocation()))
+            if (!musictype.getSound().getId().equals(this.currentMusic.getId()))
             {
-                this.mc.getSoundHandler().stop(this.currentMusic);
+                this.mc.getSoundManager().stop(this.currentMusic);
                 this.timeUntilNextMusic = 20;
             }
 
-            if (!this.mc.getSoundHandler().isPlaying(this.currentMusic))
+            if (!this.mc.getSoundManager().isPlaying(this.currentMusic))
             {
                 this.currentMusic = null;
                 this.timeUntilNextMusic = 20;
@@ -39,8 +41,8 @@ public class MusicTickHandler extends MusicTicker {
 
         if (this.currentMusic == null && this.timeUntilNextMusic-- <= 0)
         {
-        	this.currentMusic = SimpleSound.music(musictype.getSound());
-        	this.mc.getSoundHandler().play(this.currentMusic);
+        	this.currentMusic = PositionedSoundInstance.music(musictype.getSound());
+        	this.mc.getSoundManager().play(this.currentMusic);
             this.timeUntilNextMusic = 20;
         }
 	}
