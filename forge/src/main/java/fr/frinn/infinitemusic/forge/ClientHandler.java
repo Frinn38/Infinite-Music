ad2f6ae38d8d4aa55472c73dfd65bc9187a4f7d7
Frinn38 <francois.moreau3@aliceadsl.fr>
2022-12-12 16:56:19 +0100

    Update to 1.19.2 and 1.19.3

--- forge/src/main/java/fr/frinn/infinitemusic/forge/ClientHandler.java
+++ forge/src/main/java/fr/frinn/infinitemusic/forge/ClientHandler.java
@@ -5,8 +5,8 @@ import fr.frinn.infinitemusic.InfiniteMusicConfig;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.ConfigGuiHandler;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.client.event.CustomizeGuiOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -16,13 +16,13 @@ public class ClientHandler {
 
     public static void setupConfig() {
         AutoConfig.register(InfiniteMusicConfig.class, Toml4jConfigSerializer::new);
-        ModLoadingContext.get().registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class, () ->
-                new ConfigGuiHandler.ConfigGuiFactory((client, parent) -> AutoConfig.getConfigScreen(InfiniteMusicConfig.class, parent).get())
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
+                new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> AutoConfig.getConfigScreen(InfiniteMusicConfig.class, parent).get())
         );
     }
 
     @SubscribeEvent
-    public static void debugTextEvent(final RenderGameOverlayEvent.Text event) {
+    public static void debugTextEvent(final CustomizeGuiOverlayEvent.DebugText event) {
         if(!event.getLeft().isEmpty())
             InfiniteMusic.addDebugText(event.getLeft());
     }
